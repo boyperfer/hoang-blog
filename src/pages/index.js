a1603dae7d848235cefaded3865663d1fc8b369b
Tuan Nguyen <tuannguyen@Tuans-MBP.lan>
2021-05-05 17:49:32 -0500

    created a new blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,66 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from "react";
+import { Link, graphql } from "gatsby";
+import styled from "styled-components";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const BlogLink = styled(Link)`
+    text-decoration: none;
+`;
 
-export default IndexPage
+const BlogTitle = styled.h3`
+    margin-bottom: 20px;
+    color: blue;
+`;
+
+const BlogBody = styled.div`
+    margin-bottom: 50px;
+`;
+
+const IndexPage = ({ data }) => {
+    console.log(data);
+    return (
+        <Layout>
+            <div>
+                <h1>Hoang's thoughts</h1>
+                <h4>{data.allMarkdownRemark.totalCount}</h4>
+                {data.allMarkdownRemark.edges.map(({ node }) => (
+                    <BlogBody key={node.id}>
+                        <BlogLink to={node.fields.slug}>
+                            <BlogTitle>
+                                {node.frontmatter.title} -{" "}
+                                {node.frontmatter.date}{" "}
+                            </BlogTitle>
+                        </BlogLink>
+                        <p> {node.excerpt} </p>
+                    </BlogBody>
+                ))}
+            </div>
+        </Layout>
+    );
+};
+
+export default IndexPage;
+
+export const query = graphql`
+    query {
+        allMarkdownRemark {
+            totalCount
+            edges {
+                node {
+                    id
+                    excerpt
+                    frontmatter {
+                        date
+                        description
+                        title
+                    }
+                    fields {
+                        slug
+                    }
+                    html
+                }
+            }
+        }
+    }
+`;
